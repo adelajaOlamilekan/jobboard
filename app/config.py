0aa0905dbf7b889beca83aad160b4d23d59333b9
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2025-08-10 13:05:29 +0100

    Fix: Added secret key for access token generation

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # SECRET_KEY: str
+    SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     DATABASE_URL: str
     SMTP_HOST: str = None
